da54bde9855d5e42c39779c0fd9692f0fd152627
rq0 <redisk0gg@gmail.com>
2017-03-30 00:24:51 +0700

    upd travis

--- BUILD.sh
+++ BUILD.sh
@@ -3,4 +3,4 @@ mkdir -p out
 
 javac -encoding utf8 -d ./out/ -sourcepath src/ ./src/Main.java -classpath "./libs/commons-codec-1.10.jar;./libs/commons-lang3-3.5.jar;./libs/commons-cli-1.3.1.jar"
 
-jar cvmf META-INF/MANIFEST.MF untitled.jar out/
+jar cvmf src/META-INF/MANIFEST.MF untitled.jar out/
