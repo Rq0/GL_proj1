8eec20f123e43cae8f8ea9e8a96ab373ba6a72bf
rq0 <redisk0gg@gmail.com>
2017-05-02 07:01:49 +0700

    Не, ну такие ошибки были, но я ж пофиксил их. В чем дело то? Ты давай не гони, а

--- BUILD.sh
+++ BUILD.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir out #
-javac -encoding utf8 -d "./out" -sourcepath src/ ./src/main/Main.java -classpath "./libs/commons-cli-1.3.1.jar;./libs/commons-codec-1.10.jar;./libs/commons-lang3-3.5.jar;./libs/flyway-core-4.1.2.jar;./libs/h2-1.4.194.jar;./libs/log4j-core-2.8.2.jar;./libs/log4j-api-2.8.2.jar" #
 cp -R -v ./resources/* ./out #
+javac -encoding utf8 -d "./out" -sourcepath src/ ./src/main/Main.java -classpath "./libs/*" #
 echo "Class good"
 touch manifest.txt
 echo "Manifest-Version: 1.0 " > manifest.txt
