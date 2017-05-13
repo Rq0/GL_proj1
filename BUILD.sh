574a01837609a9f1ee9104cefab3898552d22a92
rq0 <redisk0gg@gmail.com>
2017-05-13 11:56:17 +0700

    форматирование скриптов

--- BUILD.sh
+++ BUILD.sh
@@ -8,7 +8,7 @@ echo "Manifest-Version: 1.0 " > manifest.txt
 echo "Created-By: rq0 " >> manifest.txt
 echo "Main-Class: main.Main " >> manifest.txt
 echo "Class-Path: out/ libs/commons-cli-1.3.1.jar libs/commons-codec-1.10.jar libs/commons-lang3-3.5.jar libs/flyway-core-4.1.2.jar libs/h2-1.4.194.jar libs/log4j-api-2.8.2.jar libs/log4j-core-2.8.2.jar" >> manifest.txt
-#Упаковать файлы в jar-архив
+#РЈРїР°РєРѕРІР°С‚СЊ С„Р°Р№Р»С‹ РІ jar-Р°СЂС…РёРІ
 jar cvfm GL_proj.jar manifest.txt ./libs/ -C out/ . #
 rm manifest.txt
 echo "Jar good"
